d62d0f712b22b7c9f6407199a7f0fe6af86f9f1f
Paul REBOH <paul@reboh.net>
2022-02-27 00:33:56 +0100

    @babel/plugin-proposal-export-namespace-from

--- src/Helpers.js
+++ src/Helpers.js
@@ -1,11 +1,4 @@
-import log from './helpers/log';
-import select from './helpers/select';
-import resolve from './helpers/resolve';
-import promiseAll from './helpers/promiseAll';
-
-export default {
-  log,
-  select,
-  resolve,
-  promiseAll,
-};
+export log from './helpers/log';
+export select from './helpers/select';
+export resolve from './helpers/resolve';
+export promiseAll from './helpers/promiseAll';
